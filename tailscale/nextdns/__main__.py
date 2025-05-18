435072118fe558035f5f68c443e1ef5a23e130f2
Andrew M. Zhang <andy@andywebservices.com>
2025-05-18 16:50:56 -0700

    Implements additional hostnames (#9)
    * Minor refactor
    
    * Removes test domain

--- tailscale/nextdns/__main__.py
+++ tailscale/nextdns/__main__.py
@@ -1,16 +1,20 @@
 import pulumi
 import pulumi_tailscale as tailscale
+
 from nextdns_provider import NextDNSRewrite
 
 config = pulumi.Config(pulumi.get_project())
 nextdns_tag = config.require('tagName')
 nextdns_profile_id = config.require('nextdnsProfileId')
+second_level_domain = config.require('secondLevelDomain')
+additionalHostnames = config.get_object('additionalHostnames', default={})
 
 ts_devices = tailscale.get_devices()
 for ts_device in ts_devices.devices:
     if nextdns_tag in ts_device.tags:
-        NextDNSRewrite(
-            src=f"{ts_device.hostname}.{config.require('mainDomain')}",
-            content=ts_device.name,
-            profile_id=nextdns_profile_id,
-        )
+        for src_hostname in [ts_device.hostname] + additionalHostnames.get(ts_device.hostname, []):
+            NextDNSRewrite(
+                src=f"{src_hostname}.{second_level_domain}",
+                content=ts_device.name,
+                profile_id=nextdns_profile_id,
+            )
