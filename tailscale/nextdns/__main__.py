a82959c01ca59a46ad2cc90159b01203361560d4
Andrew M. Zhang <andy@andywebservices.com>
2025-05-22 09:11:52 -0700

    Reorganizes ts_nextdns_rewrites

--- tailscale/nextdns/__main__.py
+++ tailscale/nextdns/__main__.py
@@ -1,21 +1,3 @@
-import pulumi
-import pulumi_tailscale as tailscale
+from ts_nextdns_rewrites import ts_nextdns_rewrites
 
-from nextdns_provider import NextDNSRewrite
-
-config = pulumi.Config(pulumi.get_project())
-nextdns_tag = config.require('tagName')
-nextdns_profile_id = config.require('nextdnsProfileId')
-second_level_domain = config.require('secondLevelDomain')
-additionalHostnames = config.get_object('additionalHostnames', default={})
-
-ts_devices = tailscale.get_devices()
-for ts_device in ts_devices.devices:
-    if nextdns_tag in ts_device.tags:
-        for src_hostname in [ts_device.hostname] + additionalHostnames.get(ts_device.hostname, []):
-            for address in ts_device.addresses:
-                NextDNSRewrite(
-                    src=f"{src_hostname}.{second_level_domain}",
-                    content=address,
-                    profile_id=nextdns_profile_id,
-                )
+ts_nextdns_rewrites()
