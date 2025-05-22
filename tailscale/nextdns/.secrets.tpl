3b0cfe45f57908e2aad3aa85b0ae9079446cc1a8
Andrew M. Zhang <andy@andywebservices.com>
2025-05-22 09:16:47 -0700

    Reorganizes ts_nextdns_rewrites (#15)
    * Reorganizes ts_nextdns_rewrites
    
    * Adds TAILSCALE_TAILNET to pulumi-ts-nd-rewrites.yml

---
+++ tailscale/nextdns/.secrets.tpl
@@ -0,0 +1,3 @@
+export NEXTDNS_API_TOKEN="op://aws-svcs/ktcu4b6pzncz464rd5owerxsku/token"
+export TAILSCALE_API_KEY="op://aws-svcs/7f67qgmdvqjl6qghuccxswbham/key"
+export TAILSCALE_TAILNET="op://aws-svcs/7f67qgmdvqjl6qghuccxswbham/hostname"
---
+++ tailscale/nextdns/ts_nextdns_rewrites.py
@@ -0,0 +1,40 @@
+import os
+
+import pulumi
+import pulumi_tailscale as tailscale
+import tldextract
+
+from nextdns_provider import NextDNSRewrite
+
+additionalHostnames = {
+
+}
+
+
+def ts_nextdns_rewrites():
+    config = pulumi.Config(pulumi.get_project())
+    nextdns_tag = config.require('tagName')
+    nextdns_profile_id = config.require('nextdnsProfileId')
+    second_level_domain = config.require('domainName')
+    tailnet_suffix = os.environ.get('TAILSCALE_TAILNET')
+    if not tailnet_suffix:
+        raise ValueError("TAILSCALE_TAILNET is required")
+
+    # Create a tldextract extractor with a custom suffix list
+    extractor = tldextract.TLDExtract(
+        extra_suffixes=[tailnet_suffix],  # treat entire tailnet as a suffix
+    )
+
+    ts_devices = tailscale.get_devices()
+    for ts_device in ts_devices.devices:
+        if nextdns_tag in ts_device.tags:
+            # nextdns tag was matched. Machine needs a machine_name.domain_name -> IP A/AAAA record
+            # machine_name = {name}.tailnet.ts.net
+            machine_name = extractor(ts_device.name).domain
+            for name in [machine_name] + additionalHostnames.get(machine_name, []):
+                for address in ts_device.addresses:
+                    NextDNSRewrite(
+                        src=f"{name}.{second_level_domain}",
+                        content=address,
+                        profile_id=nextdns_profile_id,
+                    )
